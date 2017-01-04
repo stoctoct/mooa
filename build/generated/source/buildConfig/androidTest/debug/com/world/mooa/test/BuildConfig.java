257edddb329ae0e22f0395d3587103446f8e50ab
Jang, KyungHo <stoctoct@gmail.com>
2017-01-04 15:51:09 +0900

    타이머 지우기 전 2017년 1월 4일 3시34분

---
+++ build/generated/source/buildConfig/androidTest/debug/com/world/mooa/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.world.mooa.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.world.mooa.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
